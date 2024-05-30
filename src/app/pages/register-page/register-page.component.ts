9fc919925373b7f0181f560ba0ee959f25af2fbc
Jaycom17 <juanorejuela499@gmail.com>
2024-05-30 11:20:08 -0500

    first MVP

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AsideBarComponent } from '../../components/aside-bar/aside-bar.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -9,5 +10,9 @@ import { AsideBarComponent } from '../../components/aside-bar/aside-bar.componen
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  constructor(private router: Router) {}
 
+  goToMainPage() {
+    this.router.navigate(['/']);
+  }
 }
